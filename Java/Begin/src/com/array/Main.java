61191a168313ab7686a1c928bcb723a48ce235be
LIWENHUI121017 <407325196@qq.com>
2020-04-24 16:40:06 +0800

    买卖股票的最佳时机 II 贪心算法

--- Java/Begin/src/com/array/Main.java
+++ Java/Begin/src/com/array/Main.java
@@ -6,10 +6,13 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 1, 2};
-        RemoveDuplicates rd = new RemoveDuplicates();
-        System.out.println(rd.removeDuplicates(arr));
-        System.out.println(arr.toString());
+//        int[] arr = {1, 1, 2};
+//        RemoveDuplicates rd = new RemoveDuplicates();
+//        System.out.println(rd.removeDuplicates(arr));
+//        System.out.println(arr.toString());
+        SellStocks ss = new SellStocks();
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(ss.maxProfit(arr));
     }
 
 
